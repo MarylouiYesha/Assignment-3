c9f0ee6e1038f89ef6c8ecff0e1d68aeafe529ef
user <yesha.maryloui@gmail.com>
2021-10-26 00:06:38 +0800

    updating file

---
+++ Assignment 3.py
@@ -0,0 +1,6 @@
+apple_quantity=int(input("how many many apples?"))
+orange_quantity=int(input("how many orange?"))
+apple=20
+orange=25
+apple_price= 20 * apple_quantity
+orange_price= 25 * orange_quantity

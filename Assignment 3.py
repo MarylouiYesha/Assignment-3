c761c966c25343a53ab1d7c679f69b1d7130d680
user <yesha.maryloui@gmail.com>
2021-10-26 02:40:24 +0800

    updated file

--- Assignment 3.py
+++ Assignment 3.py
@@ -1,9 +1,9 @@
 apple_quantity=int(input("enter apple quantity:"))
-total_apple_quantity=int(float(apple_quantity))
 orange_quantity=int(input("enter orange quantity:"))
-total_orange_quantity=int(float(orange_quantity))
-apple=20
-orange=25
-apple_total= 20 * total_apple_quantity
-orange_total= 25 * total_orange_quantity
-total_payment=({apple_total} + {orange_total })
+apple="20"
+orange="25"
+apple_total=20 * apple_quantity
+orange_total=25 * orange_quantity
+total_payment=apple_total
+total_payment+=orange_total
+print(f"The total amount is {total_payment}.")

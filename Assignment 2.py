8aaecf41ebbf8acf95e4b3bc256aa870da5d4891
user <yesha.maryloui@gmail.com>
2021-10-26 02:43:05 +0800

    updated file

--- Assignment 2.py
+++ Assignment 2.py
@@ -1,4 +1,4 @@
 name=input ("name:")
-age=int(input("age"))
-address=input("address")
+age=int(input("age:"))
+address=input("address:")
 print(f"Hi, my name is {name}. I am {age} years old and I live in {address}.")

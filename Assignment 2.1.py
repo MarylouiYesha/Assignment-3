a250fd37b196b7b8d6e3ef1d95db5439db76ba35
Maryloui Yesha Lacson <92633714+MarylouiYesha@users.noreply.github.com>
2021-11-11 01:34:11 +0800

    Delete Assignment 2.1.py

--- Assignment 2.1.py
+++
@@ -1,9 +0,0 @@
-apple_quantity=int(input("enter apple quantity:"))
-orange_quantity=int(input("enter orange quantity:"))
-apple="20"
-orange="25"
-apple_total=20 * apple_quantity
-orange_total=25 * orange_quantity
-total_payment=apple_total
-total_payment+=orange_total
-print(f"The total amount is {total_payment}.")

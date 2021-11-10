0cb6adfff7e0a830bae02d42ab0637daa4bae016
Maryloui Yesha Lacson <92633714+MarylouiYesha@users.noreply.github.com>
2021-11-11 01:34:25 +0800

    Delete Assignment 2.2.py

--- Assignment 2.2.py
+++
@@ -1,6 +0,0 @@
-initial_money=float(input(" Enter the amount of money you have:"))
-apple_price=float(input(" Enter price of apple:"))
-
-total_apples= int(initial_money // apple_price)
-total_change=initial_money%apple_price
-print(f"You can buy {total_apples} apple and your change is {total_change} pesos.")
